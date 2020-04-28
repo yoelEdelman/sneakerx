c3daad8f8b8a897a38b0efdc89c49814de1d14d8
Yoel Edelman <yoeledelman@gmail.com>
2020-04-28 13:08:36 +0200

    Model image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,15 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     /**
-     * The model's default values for attributes.
-     *
-     * @var array
-     */
-    protected $attributes = [
-//        'delayed' => false,
-    ];
-
-    /**
      * The attributes that aren't mass assignable.
      *
      * @var array
