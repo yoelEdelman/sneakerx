c93d4c40b91f1690aae7dfbbc75b7894d593861a
Yoel Edelman <yoeledelman@gmail.com>
2020-05-18 17:37:50 +0200

    Ajout des notifications

---
+++ resources/views/messages/error.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-danger" id="flash_msg">
+    {{ session('error') }}
+
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
---
+++ resources/views/messages/success.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-success" id="flash_msg">
+    {{ session('success') }}
+
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
